3b0eb3bb9b61da71a52306e70b74da06ea976098
ringosan1974 <wacyakun@gmail.com>
2023-12-13 01:13:52 +0900

    hogehoge

--- worldtourfinals.cpp
+++ worldtourfinals.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// WAになった。
+
+// 通った。。。
+// やっぱり深夜にプログラミングはろくなことがない
 int main() {
     int n, m;
     cin >> n >> m;
     vector<int> a(m);
-    vector<int> sa(m);
     vector<string> s(n);
     for (int i = 0; i < m; ++i) cin >> a.at(i);
     for (int i = 0; i < n; ++i) cin >> s.at(i);
-    copy(a.begin(), a.end(), sa.begin());
-    sort(sa.rbegin(), sa.rend());
+
+    //現在のプレイヤーの点数
     vector<int> p(n, 0);
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
@@ -22,33 +23,24 @@ int main() {
         }
         p.at(i) += i+1;
     }
-    vector<bool> flag(n, true);
-    vector<int> ans(n);
+
+    //それぞれのまだ解いていない問題について高い順に解いていく
+    int ma = *max_element(p.begin(), p.end());
     for (int i = 0; i < n; ++i) {
-        int ma = 0;
-        int diff;
-        for (int j = 0; j < n; ++j) {
-            if (i != j) {
-                ma = max(ma, p.at(j));
+        vector<int> sa(m);
+        copy(a.begin(), a.end(), sa.begin());
+        for (int j = 0; j < m; ++j) {
+            if (s.at(i).at(j) == 'o') {
+                sa.at(j) = 0;
             }
         }
-        diff = p.at(i) - ma;
-        if (diff > 0) {
-            ans.at(i) = 0;
-        } else if (diff == 0) {
-            ans.at(i) = 1;
-        } else {
-            int k = 0;
-            for (auto v : sa) {
-                diff += v;
-                ++k;
-                if (diff > 0) {
-                    ans.at(i) = k;
-                    break;
-                }
-            }
+        sort(sa.rbegin(), sa.rend());
+
+        int ans = 0;
+        while (p.at(i) < ma) {
+            p.at(i) += sa.at(ans++);
         }
+        cout << ans << endl;
     }
-    for (int i = 0; i < n; ++i) cout << ans.at(i) << endl;
     return 0;
 }
