849db85c14200d0fdaac146af44b8b8516b0393e
ringosan1974 <wacyakun@gmail.com>
2024-02-04 00:44:52 +0900

    hoge

---
+++ abc287/C.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; (i) < (n); ++(i))
+#define all(a) (a).begin(), (a).end()
+using ll = long long;
+
+// とりあえずDFSをためしてみる
+int main() {
+    int n, m;
+    cin >> n >> m;
+    return 0;
+}
